b52e1c63060ad96effe900ed6fb72c9b17a8b2da
WojtekK <wkokot@gmail.com>
2023-05-09 20:57:43 +0200

    Funkcje do zakodowania w ramach util

--- model/util.py
+++ model/util.py
@@ -8,3 +8,18 @@ def generate_id(number_of_small_letters=4,
                 number_of_special_chars=2,
                 allowed_special_chars=r"_+-!"):
     return 'T!uq6-b4Yq'
+
+def random_small_letters():
+    pass
+
+def random_capital_letters():
+    pass
+
+def random_digits():
+    pass
+
+def random_special_chars():
+    pass
+
+def get_unique_id():
+    pass
