19db75d6c8ff48126288fc482e353833efa9b9ea
WojtekK <wkokot@gmail.com>
2023-05-12 19:11:52 +0200

    updated sales.py

--- model/sales/sales.py
+++ model/sales/sales.py
@@ -12,3 +12,15 @@ from model import data_manager, util
 
 DATAFILE = "model/sales/sales.csv"
 HEADERS = ["Id", "Customer", "Product", "Price", "Date"]
+
+data_base = data_manager.read_table_from_file(DATAFILE)
+
+
+def list_transactions_sales():
+    data_base.insert(0, HEADERS)
+    return data_base
+
+def add_transaction_sales(table, record):
+    record.insert(0, util.generate_id())
+    table.append(record)
+    data_manager.write_table_to_file(DATAFILE, table, ";")
